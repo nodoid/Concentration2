2d71ad129f8a64f5c993e6aabe03a3fb634d8362
Paul Johnson <paul@snuberry-software.co.uk>
2017-12-29 14:33:12 +0000

    initial import

---
+++ Concentration2/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace Concentration2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.vb = new System.Windows.Forms.NumericUpDown();
+            this.cb = new System.Windows.Forms.NumericUpDown();
+            this.sb = new System.Windows.Forms.NumericUpDown();
+            this.va = new System.Windows.Forms.NumericUpDown();
+            this.ca = new System.Windows.Forms.NumericUpDown();
+            this.sa = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.vb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.va)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.vb);
+            this.groupBox1.Controls.Add(this.cb);
+            this.groupBox1.Controls.Add(this.sb);
+            this.groupBox1.Controls.Add(this.va);
+            this.groupBox1.Controls.Add(this.ca);
+            this.groupBox1.Controls.Add(this.sa);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(587, 144);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Reaction";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(489, 93);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(71, 17);
+            this.radioButton3.TabIndex = 23;
+            this.radioButton3.Text = "Unknown";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(412, 93);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(71, 17);
+            this.radioButton4.TabIndex = 22;
+            this.radioButton4.Text = "Unknown";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(184, 93);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(71, 17);
+            this.radioButton2.TabIndex = 21;
+            this.radioButton2.Text = "Unknown";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(107, 93);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 17);
+            this.radioButton1.TabIndex = 20;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Unknown";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(504, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "cm3";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(416, 44);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "mol dm-3";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(199, 44);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(27, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "cm3";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(111, 44);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(49, 13);
+            this.label12.TabIndex = 15;
+            this.label12.Text = "mol dm-3";
+            // 
+            // vb
+            // 
+            this.vb.Location = new System.Drawing.Point(489, 66);
+            this.vb.Name = "vb";
+            this.vb.Size = new System.Drawing.Size(66, 20);
+            this.vb.TabIndex = 13;
+            // 
+            // cb
+            // 
+            this.cb.DecimalPlaces = 4;
+            this.cb.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.cb.Location = new System.Drawing.Point(412, 66);
+            this.cb.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(66, 20);
+            this.cb.TabIndex = 12;
+            // 
+            // sb
+            // 
+            this.sb.Location = new System.Drawing.Point(348, 67);
+            this.sb.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.sb.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sb.Name = "sb";
+            this.sb.Size = new System.Drawing.Size(35, 20);
+            this.sb.TabIndex = 11;
+            this.sb.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // va
+            // 
+            this.va.Location = new System.Drawing.Point(184, 66);
+            this.va.Name = "va";
+            this.va.Size = new System.Drawing.Size(66, 20);
+            this.va.TabIndex = 10;
+            // 
+            // ca
+            // 
+            this.ca.DecimalPlaces = 4;
+            this.ca.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.ca.Location = new System.Drawing.Point(107, 66);
+            this.ca.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.ca.Name = "ca";
+            this.ca.Size = new System.Drawing.Size(66, 20);
+            this.ca.TabIndex = 9;
+            // 
+            // sa
+            // 
+            this.sa.Location = new System.Drawing.Point(43, 67);
+            this.sa.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.sa.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.sa.Name = "sa";
+            this.sa.Size = new System.Drawing.Size(35, 20);
+            this.sa.TabIndex = 7;
+            this.sa.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(268, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "reacts with";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(493, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Volume B";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(405, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Concentration B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(325, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Stoichiometry B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Volume A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Concentration A";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stoichiometry A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 175);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Concentration calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.vb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.va)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sa)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown vb;
+        private System.Windows.Forms.NumericUpDown cb;
+        private System.Windows.Forms.NumericUpDown sb;
+        private System.Windows.Forms.NumericUpDown va;
+        private System.Windows.Forms.NumericUpDown ca;
+        private System.Windows.Forms.NumericUpDown sa;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Concentration2/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Concentration2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int unknown;
+
+        private void calculation(List<double> vals)
+        {
+            double sta = vals[0], cna = vals[1], vla = vals[2];
+            double stb = vals[3], cnb = vals[4], vlb = vals[5];
+            double known = 0, notknown = 0;
+            if (unknown < 2)
+                known = stb == 1 ? cnb * (vlb / 1000) : (cnb / stb) * (vlb / 1000);
+            else
+                known = sta == 1 ? cna * (vla / 1000) : (cna / sta) * (vla / 1000);
+            switch (unknown)
+            {
+                // unknown [a]
+                case 0 :
+                    notknown = sta == 1 ? known*(1000/vla) : (known/sta)*(1000/vla);
+                    ca.Value = Convert.ToDecimal(notknown);
+                    break;
+                // unknown Va
+                case 1 :
+                    notknown = sta == 1 ? (known * 1000)/cna : (known*1000)/ (cna/sta);
+                    va.Value = Convert.ToDecimal(notknown);
+                    break;
+                // unknown [b]
+                case 2 :
+                    notknown = stb == 1 ? known*(1000/vlb) : (known/stb)*(1000/vlb);
+                    cb.Value = Convert.ToDecimal(notknown);
+                    break;
+                // unknown Vb
+                case 3:
+                    notknown = stb == 1 ? (1000 * known)/cnb : (1000 * known)/(cnb/stb);
+                    vb.Value = Convert.ToDecimal(notknown);
+                    break;
+            }
+
+            //double nomolesKnown = sb == 1 ? concKnown * (volKnown / 1000) : (concKnown / sb) * (volKnown / 1000);
+            //double nomolesUnknown = sa == 1 ? nomolesKnown * (1000 / volUnknown) : (nomolesKnown / sa) * (1000 / volUnknown);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<double> info = new List<double>();
+            info.Add(Convert.ToDouble(sa.Value));
+            info.Add(Convert.ToDouble(ca.Value));
+            info.Add(Convert.ToDouble(va.Value));
+            info.Add(Convert.ToDouble(sb.Value));
+            info.Add(Convert.ToDouble(cb.Value));
+            info.Add(Convert.ToDouble(vb.Value));
+            calculation(info);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            unknown = 0;
+            ca.Enabled = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            ca.Value = 0;
+            ca.Enabled = false;
+            unset(0, 1, 1, 1);
+            unknown = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            va.Value = 0;
+            va.Enabled = false;
+            unset(1, 0, 1, 1);
+            unknown = 1;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            cb.Value = 0;
+            cb.Enabled = false;
+            unset(1, 1, 0, 1);
+            unknown = 2;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            vb.Value = 0;
+            vb.Enabled = false;
+            unset(1, 1, 1, 0);
+            unknown = 3;
+        }
+
+        private void unset(int a, int b, int c, int d)
+        {
+            ca.Enabled = a == 0 ? false : true;
+            va.Enabled = b == 0 ? false : true;
+            cb.Enabled = c == 0 ? false : true;
+            vb.Enabled = d == 0 ? false : true;
+        }
+
+    }
+}
